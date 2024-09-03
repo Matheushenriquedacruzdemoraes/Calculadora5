abf2ec19d4b6e6f2ee060542994814b96bc58545
Matheushenriquedacruzdemoraes <159266966+Matheushenriquedacruzdemoraes@users.noreply.github.com>
2024-09-03 10:45:41 -0300

    Add files via upload

---
+++ Calculadora/JavaScript.js
@@ -0,0 +1,17 @@
+function calcula(opera) (
+
+let num1 = parseFloat(document.getElementById('zeroum').value);
+let num2 = parseFloat(document.getElementById('zerodois').value);
+let resultado = 0;
+if(isNaN(num1)) || isNaN(num2)){
+    alert('Divida por zero.');
+
+}else{
+
+    if(opera == '+'){
+        resultado = num1 + num2;
+    }else if()
+
+
+
+}
