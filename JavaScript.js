846731bdaccf5c17e25858033ed9c318488a6345
Matheushenriquedacruzdemoraes <159266966+Matheushenriquedacruzdemoraes@users.noreply.github.com>
2024-09-12 12:10:28 -0300

    Add files via upload

--- JavaScript.js
+++ JavaScript.js
@@ -1,17 +1,22 @@
 function calcula(opera) (
 
-let num1 = parseFloat(document.getElementById('zeroum').value);
-let num2 = parseFloat(document.getElementById('zerodois').value);
-let resultado = 0;
-if(isNaN(num1)) || isNaN(num2)){
-    alert('Divida por zero.');
-
-}else{
-
-    if(opera == '+'){
-        resultado = num1 + num2;
-    }else if()
-
-
-
-}
+    let num1 = parseFloat(document.getElementById('zeroum').value);
+    let num2 = parseFloat(document.getElementById('zerodois').value);
+    let resultado = 0;
+    if(isNaN(num1)) || isNaN(num2)){
+        alert('Divida por zero.');
+    }else{
+        if(opera == '+'){
+            resultado = num1 + num2;
+        }else if(opera == '-'){
+            resultado = num1 - num2;
+        }else if(opera == '*'){
+            resultado = num1 * num2;
+        }else if(opera == '/'){
+            resultado = num1 / num2;
+        }
+        if(resultado<0){
+            document.getElementById('resultado').style.color='red';
+        }
+        document.getElementById('resultado').textContent = 'resultado: ' + resultado;
+        }
